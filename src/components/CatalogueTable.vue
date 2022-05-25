1d35688c5e70709fc998224276f991b50bbc368b
Gabriel Mar <a329764@uach.mx>
2022-05-25 13:19:05 -0600

    added sorting to catalogue

--- src/components/CatalogueTable.vue
+++ src/components/CatalogueTable.vue
@@ -3,14 +3,16 @@
   <tbody>
       <tr id="col">
       <!-- loop through each value of the fields to get the table header -->
-      <th v-for="field in fieldsNames" :key='field' @click="sortTable(field)" >
-        {{field}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+      <th v-for="fieldName in fieldsNames" :key='fieldName' @click="sortTable(fieldName)" >
+        {{fieldName}}
+        <span v-if="sortField && sortField.fieldName === fieldName && sortField.ascendent">▲</span>
+        <span v-if="sortField && sortField.fieldName === fieldName && !sortField.ascendent">▼</span>
       </th>
       <th></th>
       <th></th>
     </tr>
       <!-- Loop through the list get the each student data -->
-      <tr id="col" v-for="item in data" :key='item'>
+      <tr id="col" v-for="item in sortedData" :key='item'>
       <td id="fil" v-for="field in fields" :key='field'>{{item[field]}}</td>
       <td id="fil"><button id="link" v-on:click="$emit('edit', item['id'])">Editar {{name}}</button></td>
       <td id="fil"><button id="link" v-on:click="$emit('delete', item['id'])">Eliminar {{name}}</button></td>
@@ -21,6 +23,35 @@
 <script>
 export default {
   name: 'TableComponent',
+  data () {
+    return {
+      sortField: null
+    }
+  },
+  computed: {
+    sortedData () {
+      if (!this.sortField) {
+        return this.data
+      }
+
+      return [...this.data].sort((a, b) => {
+        const stringA = a[this.sortField.field].toString()
+        const stringB = b[this.sortField.field].toString()
+        if (this.sortField.ascendent) {
+          return stringA.localeCompare(stringB)
+        }
+        return stringB.localeCompare(stringA)
+      })
+    }
+  },
+  methods: {
+    sortTable (fieldName) {
+      const index = this.fieldsNames.indexOf(fieldName)
+      const field = this.fields[index]
+      const ascendent = this.sortField && this.sortField.field === field ? !this.sortField.ascendent : true
+      this.sortField = { field, ascendent, fieldName }
+    }
+  },
   props: {
     data: {
       type: Array
